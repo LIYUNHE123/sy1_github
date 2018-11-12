38f90e01b23a21bd3933a4c3ec8fe722d619f747
LAPTOP-IEMAMSTO\李云鹤 <2260250953@qq.com>
2018-11-12 15:33:17 +0800

    first commit

---
+++ scripts/test_allure_xml.py
@@ -0,0 +1,18 @@
+import pytest
+
+class TestConver():
+
+    def test_hello1(self):
+        print('hello1')
+        assert 1
+
+    def test_hello2(self):
+        print('hello2')
+        assert 0
+    def test_hello3(self):
+        print('hello3')
+        assert 1
+
+    def test_hello4(self):
+        print('hello4')
+        assert 0
