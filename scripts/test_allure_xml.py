e135291bae1f0fb9f2c53c39c01ff4614c21d635
LAPTOP-IEMAMSTO\李云鹤 <2260250953@qq.com>
2018-11-12 16:13:47 +0800

    更改代码

--- scripts/test_allure_xml.py
+++ scripts/test_allure_xml.py
@@ -9,10 +9,3 @@ class TestConver():
     def test_hello2(self):
         print('hello2')
         assert 0
-    def test_hello3(self):
-        print('hello3')
-        assert 1
-
-    def test_hello4(self):
-        print('hello4')
-        assert 0
